a6b1fc0d856ce9f22bc8d7434bcf2e13046d19c5
Marcelo Valderrama <marcelovalderramacorrea@MacBook-Air-de-Marcelo.local>
2024-05-17 20:04:53 -0300

    Commit API

--- app.py
+++ app.py
@@ -1,15 +1,30 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from models.task import Task
 
 app = Flask(__name__)
 
-@app.route("/")
-def helloworld():
-    return "Hello Mundo"
+tasks = []
+task_id_control = 1
+@app.route("/tasks", methods=['POST'])
+def create_task():
+    global task_id_control
+    data = request.get_json()
+    new_task = Task(id=task_id_control,title=data['title'], description=data.get('description'))
+    task_id_control +=1
+    tasks.append(new_task)
+    print(tasks)
+    return jsonify({'message':'Nova tarefa criada com sucesso'})
 
-@app.route("/about")
-def about():
-    return "Pagina sobre"
+@app.route("/tasks", methods=['GET'])
+def get_tasks():
+    task_list = [task.to_dict() for task in tasks]
 
+    output = {
+        "tasks": task_list,
+        "total_tasks": 0
+    }
+    return jsonify(output)
 
+print(__name__)
 if __name__ == "__main__":
     app.run(debug=True)
